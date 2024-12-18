6714b3db13b42d1fcecdd90686f1146465d054ce
Nick Potts <github@nickpotts.com.au>
2024-12-18 20:21:56 +1000

    route serialization

--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,7 @@ Route::get('/auth/token/reauthenticate', function () {
 
 if (config('zaar.socialite.enabled')) {
     Route::middleware('web')->group(function () {
-        Route::get('/auth/shopify', [SocialiteController::class, 'redirect'])->name('auth.shopify');
-        Route::post('/auth/shopify', [SocialiteController::class, 'redirect'])->name('auth.shopify');
+        Route::match(['get', 'post'],'/auth/shopify', [SocialiteController::class, 'redirect'])->name('auth.shopify');
         Route::get('/auth/shopify/callback', [SocialiteController::class, 'callback'])->name('auth.shopify.callback');
     });
 }
